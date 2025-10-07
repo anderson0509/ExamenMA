ec099bbbb6640904d55fbae6e9cc23521b0c4817
anderson0509 <andersoncuellar829@gmail.com>
2025-10-06 19:41:16 -0600

    diseño formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Formulario de prueba</h1>
+    <div class="form">
+      <form >
+        <label>Nombre:</label>
+        <input type="text" />
+
+        <label>Edad:</label>
+        <input type="number"  />
+
+        <label>Dirección:</label>
+        <input type="text"  />
+
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  </div>
+</template>
